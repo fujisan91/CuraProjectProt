638c9fd6b44fdbab971d9dec711bf7a79f626aea
Kazuki Nakamura <k.nakamura@tcmobile.jp>
2019-11-07 11:19:12 +0900

    コメントをわかりやすく変更

--- components/Message.vue
+++ components/Message.vue
@@ -1,25 +1,25 @@
 <template>
   <div>
+    <!-- ⓶propsで受け取ったmessagesをループしてmessageとindexを取り出す -->
     <div
       class="message"
-      ⓶！！ここにメッセージをループする処理を追加
       :key="index"
       :class="{own: message.user.id == ユーザーのIDと比較 && message.user.username == ユーザーの名前と比較}"
     >
       <div style="margin-top: 9px"></div>
+      <!-- ⓷ここの?を編集して条件処理を追加 -->
       <div
         class="username"
-        ⓷ここの?を編集して条件処理を追加
         ???="index>0 && messages[index-1].user.username != message.user.username"
       >
-        <!-- ユーザーの名前を表示 -->
+        <!-- ここにユーザーの名前を表示 -->
       </div>
       <div class="username" v-if="index == 0">
-        <!-- ユーザーの名前を表示 -->
+        <!-- ここにユーザーの名前を表示 -->
       </div>
       <div class="content">
         <!-- ⓸メッセージの内容を表示するためにv-htmlにメッセージの内容を渡す。 -->
-        <div v-html></div>
+        <div></div>
       </div>
     </div>
   </div>
@@ -27,7 +27,10 @@
 
 <script>
 export default {
+  // 親コンポーネントからpropsでmessagesを受け取っている
   props: ['messages'],
+
+  // computedを使うと算出後のデータをdataとして扱える。つまり算出結果にthis.userとアクセスできる
   computed: {
     user() {
       if (this.$store.state.AuthModule.user === null) {
